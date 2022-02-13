6c8a3676b25274c763be6957987e04a10152c73d
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-02-13 14:54:43 +0300

    Start of migrations

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuraion
     """
     SECRET_KEY= os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://marc:qwerty01@localhost/database'
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://marc:qwerty01@localhost/users'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 class Production(Config):
     """
