bbec1ba4c38a6298340cfe20566f571eb58a89f7
Marc-Mwangi <marc.mwangi@student.moringaschool.com>
2022-02-13 12:31:19 +0300

    Setting up database

--- app/models.py
+++ app/models.py
@@ -7,21 +7,4 @@ class User(db.Model):
     
     def __repr__(self):
         return f'User{self.username}'
-    #class variable for user data
-    user_email=[]
-    user_password=[]
-    #initializing class
-    def  __init__(self, firstName, lastName, email, password , password2):
-        self.f_Name = firstName
-        self.l_name= lastName
-        self.email = email
-        self.password = password
-        self.password2 = password2 
-    
-    #Saving  user email
-    def save_email(self):
-        User.user_email.append(self.email)
-
-    #Saving user password
-    def save_password(self):
-        User.user_password.append(self.password)
+  
